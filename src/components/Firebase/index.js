aa5b62a8e36bade339da3bd0647d9e4cc54a7926
ryan-rayark <48552331+ryan-rayark@users.noreply.github.com>
2019-06-02 07:35:33 +0530

    adding env var

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -2,14 +2,15 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
   // Your web app's Firebase configuration
+  {FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DATABASE_URL, FIREBASE_STORAGE_BUCKET, FIREBASE_PROJECT_ID, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID}=process.env
   var firebaseConfig = {
-    apiKey: "AIzaSyBZXspwx1TmrNE0ULmUGcDijN2spaG7-VQ",
-    authDomain: "image-upload-form.firebaseapp.com",
-    databaseURL: "https://image-upload-form.firebaseio.com",
-    projectId: "image-upload-form",
-    storageBucket: "image-upload-form.appspot.com",
-    messagingSenderId: "312767696860",
-    appId: "1:312767696860:web:9d626f3f2ba4a79a"
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN ,
+    databaseURL: FIREBASE_DATABASE_URL,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
