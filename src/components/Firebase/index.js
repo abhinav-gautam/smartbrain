39c799d3876b49d9ffd808824ed4b8ade439e53b
ryan-rayark <48552331+ryan-rayark@users.noreply.github.com>
2019-06-04 13:55:35 +0530

    update

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -8,7 +8,7 @@ import 'firebase/storage';
     authDomain: process.env.FIREBASE_AUTH_DOMAIN ,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    storageBucket: "image-upload-form.appspot.com",
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.FIREBASE_APP_ID
   };
